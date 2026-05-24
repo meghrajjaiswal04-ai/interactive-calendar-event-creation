6f8e972b989e4c12e92efba46422d3a06f3de7e1
meghrajjaiswal04-ai <meghrajjaiswal04@gmail.com>
2026-05-24 13:06:57 +0530

    Angular event calendar project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
-import { EventComponent } from './event/event';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EventService } from './services/event.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [EventComponent],
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
+export class App implements OnInit {
+
+  events: any[] = [];
+
+  constructor(private eventService: EventService) {}
+
+  ngOnInit(): void {
+    this.loadEvents();
+  }
+
+  loadEvents() {
+    this.eventService.getEvents().subscribe(data => {
+      this.events = data;
+    });
+  }
+
+  addEvent() {
+
+    const newEvent = {
+      title: 'New Event',
+      date: '2025-06-01'
+    };
+
+    this.eventService.addEvent(newEvent).subscribe(() => {
+      this.loadEvents();
+    });
+  }
+
+  deleteEvent(id: number) {
+    this.eventService.deleteEvent(id).subscribe(() => {
+      this.loadEvents();
+    });
+  }
 }
